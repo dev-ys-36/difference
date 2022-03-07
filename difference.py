6d64334896467ad8a8f61e56b2205794362576b0
dev-ys-36 <98698629+dev-ys-36@users.noreply.github.com>
2022-03-08 00:07:37 +0900

    Add files via upload

---
+++ difference.py
@@ -0,0 +1,18 @@
+tuple_ = (1, 2, 3, 4, 5)
+tuple__ = 1, 2, 3, 4, 5
+
+list_ = [1, 2, 3, 4, 5]
+
+set_ = {1, 2, 3, 4, 5} # not list
+
+dictionary_ = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5} # key , value
+
+print(type(tuple_))
+
+print(type(tuple__))
+
+print(type(list_))
+
+print(type(set_))
+
+print(type(dictionary_))
